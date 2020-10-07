3b1f7cbaa92060aa052f8d84d38e9d00057769fd
ygrshk <ygrshk@gmail.com>
2020-10-08 00:55:22 +0900

    modify static setting

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ try:
     from .local_settings import *
 except ImportError:
     STATIC_URL = '/static/'
-    # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
