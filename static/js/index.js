1e50eff280d1312e7b56a1de1bf77cae2da21fa2
ygrshk <ygrshk@gmail.com>
2020-10-11 23:58:22 +0900

    fix reply

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,27 @@
+if(performance.navigation.type == 1 && location.hash){
+  location.href = location.href.replace(/#.*/, "");
+}
 $(function(){
   $('.btn-trigger').on('click', function() {
     $(this).toggleClass('active');
     $('.menu_wrapper').toggleClass('active');
+    return false;
+  });
+});
+
+$(function(){
+  $('a[href^="#"]').click(function() {
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+
+
+    // var id = document.getElementById(href);
+    // var urlHash = location.hash;
+    $(`${href} .content`).eq(0).css( { 'background-color' : '#bbbbbb' });
+
     return false;
   });
 });
