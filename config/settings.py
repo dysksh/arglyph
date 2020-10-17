9c99350260fd26a6cb21454559db2588dc660b99
ygrshk <ygrshk@gmail.com>
2020-10-17 18:52:17 +0900

    [fix]refs #1 static_root削除

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,6 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 AUTH_USER_MODEL = 'account.User'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
